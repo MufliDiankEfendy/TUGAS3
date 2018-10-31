265cd1952eb6775f9baff29681cee1f4de7d74bd
MufliDiankEfendy <43692429+MufliDiankEfendy@users.noreply.github.com>
2018-10-31 22:58:24 +0700

    Create classmainqueuestring.java

---
+++ classmainqueuestring.java
@@ -0,0 +1,16 @@
+public class classmainqueuestring {
+
+	
+	public static void main(String[] args) {
+		classqueuestring q=new classqueuestring();
+		q.cetak();
+		q.insert("MUFLI");
+		q.cetak();
+		q.insert("DIANK");
+		q.cetak();
+		q.insert("EFENDY");
+		q.cetak();
+
+	}
+
+}
